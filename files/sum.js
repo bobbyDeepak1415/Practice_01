5d88c46fdb8f37ef44fc826a36b58d83ef3d5cf9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-07-06 17:33:28 +0530

    first

---
+++ files/sum.js
@@ -0,0 +1,11 @@
+
+function sum(a,b){
+    return a+b
+}
+
+module.exports=sum
+
+
+function display(){
+    console.log("gello")
+}
---
+++ files/sum.test.js
@@ -0,0 +1,6 @@
+const sum=require('./sum')
+
+
+test("should add the two nums correctly",()=>{
+expect(sum(1,2)).toBe(3)
+})
