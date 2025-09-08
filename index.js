416e6331b7eea58211df86207513e154abf2c4e7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-08 10:36:40 +0530

    practiced array destructuring

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-// debounce
 
-const input = document.querySelector("input");
-const clicked = document.querySelector(".clicked");
-const run = document.querySelector(".func_run");
 
-let clickedTimes = 0;
-let runTimes = 0;
+const arr1=["apple","orange","grapes","pineapple","kiwi"]
 
-const myDebounce = (func, delay) => {
-  let timer = 0;
-  return function (...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
+const [one,two,...rest]=arr1
 
-const debounceFunction = myDebounce(() => {
-  run.innerHTML = ++runTimes;
-}, 1000);
-
-input.addEventListener("input", () => {
-  clicked.innerHTML = ++clickedTimes;
-  debounceFunction();
-});
+console.log(one)
