58761da60637013696170093a0deedac5ae04709
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-08 10:32:40 +0530

    debounce functionality added to the index.js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+// debounce
 
+const input = document.querySelector("input");
+const clicked = document.querySelector(".clicked");
+const run = document.querySelector(".func_run");
 
+let clickedTimes = 0;
+let runTimes = 0;
 
+const myDebounce = (func, delay) => {
+  let timer = 0;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
 
+const debounceFunction = myDebounce(() => {
+  run.innerHTML = ++runTimes;
+}, 1000);
 
+input.addEventListener("input", () => {
+  clicked.innerHTML = ++clickedTimes;
+  debounceFunction();
+});
