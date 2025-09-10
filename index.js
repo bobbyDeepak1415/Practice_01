ceb77533f905df67f97f8933c6a9100b73c10567
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-10 13:20:24 +0530

    practiced debouncing and throttling

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const input = document.querySelector("input");
+const button = document.querySelector("button");
+const btnClicked = document.querySelector(".clicked");
+const functionRun = document.querySelector(".func_run");
 
-console.log("hello");
+let clickedTimes = 0;
+let runTimes = 0;
+
+const myThrottle = (func, delay) => {
+  let lastRun = 0;
+  return function (...args) {
+    let now = Date.now();
+    if (now - lastRun >= delay) {
+      lastRun = now;
+      func(...args);
+    }
+  };
+};
+
+const funcThrottle = myThrottle(() => {
+  functionRun.innerHTML = ++runTimes;
+}, 800);
+button.addEventListener("click", () => {
+  btnClicked.innerHTML = ++clickedTimes;
+  funcThrottle();
+});
