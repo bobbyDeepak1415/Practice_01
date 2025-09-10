222a7b266eb824ba43a003661d66ac3fcf3a1afe
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-11 00:35:30 +0530

    includes method

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 
 
-const person1={
-fname:"Bobby",
-lname:"deepak"
-
-}
+const fruits=["apples","oranges","kiwis"]
 
 
-function display(state,country){
-  return this.fname+" "+this.lname+" is from "+state+" in "+country
-}
 
+const result=fruits.find(item=>item.includes("is"))
 
-const result=display.bind(person1,"ap","india")
-console.log(result())
+console.log(result)
