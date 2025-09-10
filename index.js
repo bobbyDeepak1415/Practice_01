08114cc78497d71eba578f610f85072897efc146
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-11 00:27:19 +0530

    practiced call,apply,bind

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 
+const person1={
+fname:"Bobby",
+lname:"deepak"
+
+}
+
+
+function display(state,country){
+  return this.fname+" "+this.lname+" is from "+state+" in "+country
+}
+
+
+const result=display.bind(person1,"ap","india")
+console.log(result())
