a2dfae5c87d150669d79882e25e7a43d77ddfa81
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-10 23:43:51 +0530

    revising array methods

--- index.js
+++ index.js
@@ -1,25 +1,2 @@
-const button = document.querySelector("button");
-const btnClicked = document.querySelector(".clicked");
-const functionRun = document.querySelector(".func_run");
 
-let clickedTimes = 0;
-let runTimes = 0;
 
-const myThrottle = (func, delay) => {
-  let lastRun = 0;
-  return function (...args) {
-    let now = Date.now();
-    if (now - lastRun >= delay) {
-      lastRun = now;
-      func(...args);
-    }
-  };
-};
-
-const funcThrottle = myThrottle(() => {
-  functionRun.innerHTML = ++runTimes;
-}, 800);
-button.addEventListener("click", () => {
-  btnClicked.innerHTML = ++clickedTimes;
-  funcThrottle();
-});
