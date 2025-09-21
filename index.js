50ee17465f0a3078d2393e318c71d9b826cc133a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-21 22:29:03 +0530

    practice throttling

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const input = document.querySelector("input");
-const typed = document.querySelector(".typed");
+const input = document.querySelector("button");
+const clicked = document.querySelector(".clicked");
 const func_Run = document.querySelector(".func_run");
 
 let typedTimes = 0;
 let functionRunTimes = 0;
 
-const myDebounce = (func,delay) => {
-  let timer = 0;
-  return function (...args) {
-    if (timer) {
-      clearTimeout(timer);
-    }
 
-    timer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
-
-const debounceFunc = myDebounce(() => {
-  func_Run.innerHTML = ++functionRunTimes;
-}, 1200);
-
-document.addEventListener("input", () => {
-  typed.innerHTML = ++typedTimes;
-  debounceFunc();
-});
+document.addEventListener("click",()=>{
+    
+})
