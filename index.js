6fa7c70e60232c8a308a4059955bfc114d829c3d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-21 22:36:36 +0530

    throttling done

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const input = document.querySelector("button");
 const clicked = document.querySelector(".clicked");
 const func_Run = document.querySelector(".func_run");
 
-let typedTimes = 0;
+let clickedTimes = 0;
 let functionRunTimes = 0;
 
+const myThrottle = (func, delay) => {
+  let lastRun = 0;
+  return function (...args) {
+    let now = Date.now();
 
-document.addEventListener("click",()=>{
-    
-})
+    if (now - lastRun > delay) {
+      lastRun = now;
+      func(...args);
+    }
+  };
+};
+
+const functionThrottle = myThrottle(() => {
+  func_Run.innerHTML = ++functionRunTimes;
+}, 1200);
+
+document.addEventListener("click", () => {
+  clicked.innerHTML = ++clickedTimes;
+  functionThrottle();
+});
