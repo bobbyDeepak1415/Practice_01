fc90bf07b338a0367bdb38443a6afad643d01431
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-21 22:15:46 +0530

    practice debugging

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const input = document.querySelector("input");
+const typed = document.querySelector(".typed");
+const func_Run = document.querySelector(".func_run");
 
+let typedTimes = 0;
+let functionRunTimes = 0;
 
-const fruits=["apples","oranges","kiwis"]
+const myDebounce = (...args) => {
+  let timer = 0;
+  return function (...delay) {
+    if (timer) {
+      clearTimeout(timer);
+    }
 
+    timer = setTimeout(() => {
+      func_Run(...args);
+    }, delay);
+  };
+};
 
+const debounceFunc =
+  (() => {
+    func_Run.innerHTML = functionRunTimes++;
+  },
+  1200);
 
-const result=fruits.find(item=>item.includes("is"))
-
-console.log(result)
+document.addEventListener("input", () => {
+  typed.innerHTML = ++typedTimes;
+  debounceFunc();
+});
