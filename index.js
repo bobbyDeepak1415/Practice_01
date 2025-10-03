a7e0cb2442a4187bb74bbd618a3104c43d780e35
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-04 03:33:02 +0530

    re-learning switch

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+const arr1 = ["apple", "mangoes", "grapes", "oranges"];
+
+function display(arr) {
+  const local = [...arr1];
+  const random = Math.floor(Math.random() * local.length - 1);
+  return local[random];
+}
 
 
+
+switch(display(arr1)){
+    case "apple":
+    console.log("apples are good")
+    break
+    case "mangoes":
+    console.log("mangoes are the best")
+    break
+    case "grapes":
+    console.log("grapes are ok")
+    break
+    case "oranges":
+    console.log("oranges are like mangoes")
+    break
+    default :
+    console.log("fruits are good")
+
+}
