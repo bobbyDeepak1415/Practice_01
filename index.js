38ee49dc2a56d4502ed27c175ff3c219ca8f7788
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-04 03:10:40 +0530

    re-learning switch

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const animal = "shark";
 
-switch (animal) {
-  case "cat":
-    console.log("cats are fine ");
-    break;
-  case "dogs":
-    console.log("dogs are good");
-    break;
-  case "shark":
-    console.log("sharks are a no no");
-    break;
 
-  default:
-    console.log("animals are good");
-}
