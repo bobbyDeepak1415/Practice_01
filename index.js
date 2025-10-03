f141fbcda3f143ef96bd277d8e71b4d1a1ccd3aa
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-04 02:41:25 +0530

    re-learning switch

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-const input = document.querySelector("button");
-const clicked = document.querySelector(".clicked");
-const func_Run = document.querySelector(".func_run");
 
-let clickedTimes = 0;
-let functionRunTimes = 0;
 
-const myThrottle = (func, delay) => {
-  let lastRun = 0;
-  return function (...args) {
-    let now = Date.now();
-
-    if (now - lastRun > delay) {
-      lastRun = now;
-      func(...args);
-    }
-  };
-};
-
-const functionThrottle = myThrottle(() => {
-  func_Run.innerHTML = ++functionRunTimes;
-}, 1200);
-
-document.addEventListener("click", () => {
-  clicked.innerHTML = ++clickedTimes;
-  functionThrottle();
-});
+// you are an expert in flute and and its tune nodes and swars and how to break down a tune into nodes
