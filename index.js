a3a2d7e34d1d1bf2a895c262d59236c1caf4e65c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-23 17:24:45 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const arr1 = ["apple", "mangoes", "grapes", "oranges"];
 
-function display(arr) {
-  const local = [...arr1];
-  const random = Math.floor(Math.random() * local.length - 1);
-  return local[random];
-}
+let str="success"
 
 
+function display(str){
 
-switch(display(arr1)){
-    case "apple":
-    console.log("apples are good")
-    break
-    case "mangoes":
-    console.log("mangoes are the best")
-    break
-    case "grapes":
-    console.log("grapes are ok")
-    break
-    case "oranges":
-    console.log("oranges are like mangoes")
-    break
-    default :
-    console.log("fruits are good")
+    let obj={}
 
 }
+
+
+console.log(display(str))
