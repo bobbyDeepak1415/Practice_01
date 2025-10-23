685a57764f774845691b79fb23d4b2de8b5787da
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-24 02:55:34 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-.var x = 1 a(); b(); console.log(x); function a(){ var x=10; console.log(x); function b(){ var x=100
-console.log(x); }
+var x = 1;
+
+
+a(); //10
+b(); //100
+
+console.log(x); //1
+function a() {
+  var x = 10;
+  console.log(x); //10
+}
+function b() {
+  var x = 100;
+  console.log(x); //100
+}
+
