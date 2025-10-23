ed075d6dccf431bbe34e4f3187a722178ab1ab0d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-24 02:20:10 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ let str="furry"
 
 function display(str){
 
+    if(str.length===0) return
+
+    let obj={}
+
+    for(let i=0;i<str.length;i++){
+        let char=str[i]
+        if(obj[char]) return char
+        obj[char]=true
+    }
+
+    return null
+
 }
 
 console.log(display(str))
