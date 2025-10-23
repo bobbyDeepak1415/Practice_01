3f0353c4fa3a6fb23528912952c5f26211b01d22
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-24 02:32:42 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -5,27 +5,23 @@ const run = document.querySelector(".func_run");
 let clickedTimes = 0;
 let runTimes = 0;
 
+const myThrottle = (func, delay) => {
+  let lastRun = 0;
 
+  return function () {
+    let timeNow = Date.now();
+    if (timeNow - lastRun > delay) {
+      lastRun = timeNow;
+      func();
+    }
+  };
+};
 
-const myThrottle=(func,delay)=>{
-
-    let lastRun=0
-
-    let timeNow=Date.now()
-   if(timeNow-lastRun>delay){
-    lastRun=timeNow
-    func()
-   }
-
-}
-
-
-
-const throttleFunc = myThrottle(() => {
+const ThrottleFunc = myThrottle(() => {
   run.innerHTML = runTimes++;
 }, 1200);
 
 btn.addEventListener("click", () => {
   clicked.innerHTML = clickedTimes++;
-  throttleFunc();
+  ThrottleFunc();
 });
