cc88664ce6de5c7d645322d5d021ca072127a77d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-24 03:10:36 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-let test = true;
-let counter = 0;
-setTimeout(() => {
-  test = false;
-}, 2000);
-setInterval(() => {
-  if (test) {
-    console.log(counter++);
-  }
-}, 1000);
+setTimeout(() => console.log("timeout"), 1000);
+setInterval(() => console.log("interval"), 1000);
