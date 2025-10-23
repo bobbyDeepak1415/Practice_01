7c19ea92a529228c3baca97e0fe76a6e9b11bea8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-23 17:34:33 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-let str = "success";
 
-function display(str) {
-  let obj = {};
+function sum(a){
+    return function(b){
+        return function(c){
+            
+            return function(){
 
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (obj[char]) return char;
-    obj[char] = true;
-  }
+                
+                return a+b+c
+            }
 
-  return null;
+        }
+    }
 }
 
-console.log(display(str));
+const result=sum
+const result1=result
+const result2=result1
+const result3=result2
+
+console.log(result3(3)(2)(4)())
