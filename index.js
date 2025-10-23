0f9e36535af6822afe01f459b562ac97c410ad5c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-24 02:23:42 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const btn=document.querySelector("button")
-const clicked=document.querySelector(".clicked")
-const run=document.querySelector(".func_run")
+const btn = document.querySelector("button");
+const clicked = document.querySelector(".clicked");
+const run = document.querySelector(".func_run");
 
+let clickedTimes = 0;
+let runTimes = 0;
 
+// const debounce
 
+btn.addEventListener("click", () => {
+  clicked.innerHTML = clickedTimes++;
+  run.innerHTML = runTimes++;
+});
