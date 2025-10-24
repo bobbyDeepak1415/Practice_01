dd2a108c8d61b005eb2f6fcdcfb77715ca4835fe
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-24 17:48:08 +0530

    strings ques 8

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-
-
-function display1(){
-
-setTimeout(()=>{
-console.log("from 1")
-},1000)
+function display1() {
+  return new Promise((resolve) => {
+    setTimeout((resolve) => {
+      console.log("from 1");
+      
+    }, 1000);
+  });
 }
 
-
-function display2(){
-   setTimeout(() => {
-     console.log("from 2");
-   }, 1100);
+function display2() {
+  setTimeout(() => {
+    console.log("from 2");
+  }, 1100);
 }
-function display3(){
-   setTimeout(() => {
-     console.log("from 3");
-   }, 1100);
+function display3() {
+  setTimeout(() => {
+    console.log("from 3");
+  }, 1100);
 }
 
-
-
-
-async function display(){
-    const result1=await display1()
+async function display() {
+  try {
+    const result1 = await display1();
+    const result2 = await display2();
+    const result3 = await display3();
+  } catch (error) {
+    console.log("not found");
+  }
 }
+
+display()
