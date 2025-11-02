af941438758bf1db8c7562f7fdba25400a394c31
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-03 03:27:00 +0530

    setting up

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
-function display1() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("from 1");
-      resolve()
-      
-    }, 1000);
-  });
-}
-
-function display2() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("from 2");
-      resolve();
-    }, 1100);
-  });
-}
-
-function display3() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("from 3");
-      resolve();
-    }, 1100);
-  });
-}
-async function display() {
-  try {
-    const result1 = await display1();
-    const result2 = await display2();
-    const result3 = await display3();
-    return "all done"
-  } catch (error) {
-    console.log("not found");
+setTimeout(() => {
+  for (let i = 1; i <= 3; i++) {
+    console.log(i);
   }
-}
-
-display().then((res)=>{
-    console.log(res)
-})
+}, 1000);
