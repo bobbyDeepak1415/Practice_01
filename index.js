559ad703158abef144949a7dea106c5cc7b62e99
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-03 03:43:22 +0530

    setting up

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-
-
-function outerFunc(outerVariable){
-  return function innerFunc(innerVariable){
-    return function innnnerFunc(innnnerVariable){
-      return function oneMore(){
-
-        return outerVariable+innerVariable+innnnerVariable
-      }
-
-    }
-  }
+function outerFunc(outerVariable) {
+  return function innerFunc(innerVariable) {
+    return function innnnerFunc(innnnerVariable) {
+      return outerVariable + innerVariable + innnnerVariable;
+    };
+  };
 }
 
+const newFunc = outerFunc;
+const newerFunc = newFunc;
+const oneMore = newerFunc;
 
+console.log(oneMore("000")("111")("222"));
 
-
-
-
-console.log(outerFunc("000")("111")("222")());
+// console.log(outerFunc("000")("111")("222"));
