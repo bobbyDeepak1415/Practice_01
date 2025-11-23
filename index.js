f01d3e266839d87010a834a1dfac4fe7f0d41896
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-23 23:37:49 +0530

    changes made

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
 
 
 
-const input = document.querySelector("input");
-const functionClicked = document.querySelector(".clicked");
-const functionRun = document.querySelector(".func_run");
-
-let clickedTimes = 0;
-let runTimes = 0;
-
-const myDebounce = (func, delay) => {
-  let timer = 0;
-  return function (...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
-
-const functionDebounce = myDebounce(() => {
-  functionRun.innerHTML = ++runTimes;
-}, 1000);
-
-input.addEventListener("input", () => {
-  functionClicked.innerHTML = ++clickedTimes;
-  functionDebounce();
-});
