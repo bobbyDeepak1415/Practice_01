cb1f6b0ca01e30d9299f25aae33e565911cf6dea
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-05 14:50:31 +0530

    changes made

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ let person1 = {
   name: "Deepak",
   age: 29,
   active: true,
-  adress:{
-    city:"bengaluru",
-    code:101
-  }
+  adress: {
+    city: "bengaluru",
+    code: 101,
+  },
 };
 
-let { name:title, adress:{city:sheher,code} } = person1;
+let { title: name } = person1;
 
 console.log(title);
-// console.log(age);
-// console.log(active);
-
-console.log(sheher);
-
