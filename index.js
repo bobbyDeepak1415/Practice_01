2cf395672956db396fed1448c8e5553234e3dec0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-05 14:45:09 +0530

    changes made

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
+let person1 = {
+  name: "Deepak",
+  age: 29,
+  active: true,
+};
 
+let { name, age, active } = person1;
 
-let arr1=["Bobby","Deepak","Leo","Kingman"]
-let arr=[5]
-
-let [x=10,y=20]=arr
-
-console.log(x)
-
-
-// let [title,...remaining]=arr1
-
-// console.log(title)
-// console.log(remaining)
-
-
-
-// let person1={
-//     name:"Deepak",
-//     age:29,
-//     active:true
-// }
-
-
-// let {title}=person1
-
-
-// console.log(title)
-// console.log(years)
-// console.log(status)
-
+console.log(name);
+console.log(age);
+console.log(active);
