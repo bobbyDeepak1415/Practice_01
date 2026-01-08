d00baa625076ec1b876ce2cef13097e470580528
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-08 23:11:42 +0530

    debugging

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const a = 10;
-console.log(a);
-
-setTimeout(() => {
-  console.log("timeout");
-
-  Promise.resolve().then(() => {
-    console.log("promise");
-  });
-}, 0);
-
-console.log("end");
 
 
-// 10,end,timeout,promise
+
