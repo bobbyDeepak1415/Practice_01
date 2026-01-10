a608eac452f38dde5fb81f9ef516f905d3b871d7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-11 00:47:29 +0530

    debugging

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const clicked=document.querySelector(".clicked")
 const run=document.querySelector(".func_run")
 
 
+let clickedTimes=0
+let runTimes=0
+
+
+const myDebounce=(0=>{
+
+})
+
+const debounce=myDebounce(()=>{
+
+    run.innerHTML=++runTimes
+
+},1200)
 
 
 
 
 document.addEventListener("input",()=>{
-    console.log(input.innerText)
+    clicked.innerHTML=++clickedTimes
+
 })
