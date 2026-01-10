d473bd81970c3e584e16c915b2eb0f3efd9e1e9a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-11 00:44:33 +0530

    debugging

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 
+// debounce
+
+const input=document.querySelector("input")
+const clicked=document.querySelector(".clicked")
+const run=document.querySelector(".func_run")
 
 
+
+
+
+
+document.addEventListener("input",()=>{
+    console.log(input.innerText)
+})
