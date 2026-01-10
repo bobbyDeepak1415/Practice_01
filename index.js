cbe462f0d5ae22c86fb0313951eb63ce5c57fc04
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-11 00:49:42 +0530

    debugging

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-
 // debounce
 
-const input=document.querySelector("input")
-const clicked=document.querySelector(".clicked")
-const run=document.querySelector(".func_run")
+const input = document.querySelector("input");
+const clicked = document.querySelector(".clicked");
+const run = document.querySelector(".func_run");
 
+let clickedTimes = 0;
+let runTimes = 0;
 
-let clickedTimes=0
-let runTimes=0
+const myDebounce = (func, delay) => {
+  let timer = 0;
 
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
 
-const myDebounce=(func,delay)=>{
-let timer=0
+const debounce = myDebounce(() => {
+  run.innerHTML = ++runTimes;
+}, 1200);
 
-return function(...args){
-clearTimeout(timer)
-timer=setTimeout(()=>{
-    func(...args)
-},delay)
-}
-
-}
-
-const debounce=myDebounce(()=>{
-
-    run.innerHTML=++runTimes
-
-},1200)
-
-
-
-
-document.addEventListener("input",()=>{
-    clicked.innerHTML=++clickedTimes
-
-})
+document.addEventListener("input", () => {
+    clicked.innerHTML = ++clickedTimes;
+});
