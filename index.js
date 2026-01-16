a40ca5938b9704a4d81813fcb8692dd05cf1f4ae
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-16 17:13:47 +0530

    set-up done

---
+++ index.js
@@ -0,0 +1,40 @@
+const alphabets = {
+  d: {
+    name: "dd",
+    series: 4,
+  },
+  k: {
+    name: "k",
+    series: 3,
+  },
+  p: {
+    name: "p",
+    series: 2,
+  },
+  n: {
+    name: "n",
+    series: 7,
+  },
+  l: {
+    name: "l",
+    series: 1,
+  },
+  a: {
+    name: "a",
+    series: 12,
+  },
+  o: {
+    name: "o",
+    series: 5,
+  },
+  m: {
+    name: "m",
+    series: 8,
+  },
+  b: {
+    name: "b",
+    series: 8,
+  },
+};
+
+console.log(Object.keys(alphabets))
