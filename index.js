ce634d0f4d53285a08091c552a44ca5046713e1b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-11 16:11:58 +0530

    setup done

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-const alphabets = {
-  d: {
-    name: "d",
-    series: 4,
-  },
-  k: {
-    name: "k",
-    series: 3,
-  },
-  p: {
-    name: "p",
-    series: 2,
-  },
-  n: {
-    name: "n",
-    series: 7,
-  },
-  l: {
-    name: "l",
-    series: 1,
-  },
-  a: {
-    name: "a",
-    series: 12,
-  },
-  o: {
-    name: "o",
-    series: 5,
-  },
-  m: {
-    name: "m",
-    series: 8,
-  },
-  b: {
-    name: "b",
-    series: 8,
-  },
-};
 
-console.log(
-  Object.values(alphabets)
-    .sort((a, b) => a.name.localeCompare(b.name))
-    .map((item) => item.name)
-);
+
+
+...
