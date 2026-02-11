469dac1b8fdfc9382046f2f311cb0cf954f7c098
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-11 16:19:30 +0530

    setup done

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+console.log(name);
+console.log(title);
+console.log(firstName);
+
+var name
+let title=="Bobby"
+title
+// const firstName
+
+name="Leo"
 
 
+// firstName="Bob"
 
+// console.log("hello");
