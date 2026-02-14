71d1b53dc2f3ef1c85ad61e11474ac2ad5a00aa2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-14 21:38:05 +0530

    setup done

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-console.log("A");
-async function test() {
-  console.log("B");
-await Promise.resolve();
-  console.log("C");
-  Promise.resolve();
-  console.log("D");
-}
-test();
-Promise.resolve().then(() => console.log("E"));
-setTimeout(() => console.log("F"), 0);
-console.log("G");
 
 
-// A,B,G,C,D,E,F
+
+// (A, G, B, C, D, E, F);
