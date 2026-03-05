d7cac4b8c9d201cca1e148afd0a10423e894f678
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-05 16:22:22 +0530

    setup done

---
+++ Untitled-1.jsx
@@ -0,0 +1,17 @@
+// const products = [
+//   { id: 1, name: "MacBook Pro" },
+//   { id: 2, name: "iPhone 15" },
+//   { id: 3, name: "Samsung Galaxy" },
+//   { id: 4, name: "Dell XPS" },
+//   { id: 5, name: "HP Pavilion" },
+//   { id: 6, name: "iPad Air" },
+//   { id: 7, name: "Sony Headphones" },
+//   { id: 8, name: "Apple Watch" },
+//   { id: 9, name: "Google Pixel" },
+//   { id: 10, name: "Asus ROG" },
+//   { id: 11, name: "Lenovo ThinkPad" },
+//   { id: 12, name: "AirPods Pro" },
+//   { id: 13, name: "Microsoft Surface" },
+//   { id: 14, name: "OnePlus Nord" },
+//   { id: 15, name: "Canon Camera" },
+// ];
--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+// console.log("A");
+// async function test() {
+//   console.log("B");
+// await Promise.resolve();
+//   console.log("C");
+//   Promise.resolve();
+//   console.log("D");
+// }
+// test();
+// Promise.resolve().then(() => console.log("E"));
+// setTimeout(() => console.log("F"), 0);
+// console.log("G");
 
 
+first()
 
-// (A, G, B, C, D, E, F);
+var first=()=>{
+
+}
+
