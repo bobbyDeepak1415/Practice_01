c5893b6e6b496f83769d1a030ccb7789b17b7287
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-11 16:45:12 +0530

    setup done

--- index.js
+++ index.js
@@ -11,3 +11,22 @@
 // Promise.resolve().then(() => console.log("E"));
 // setTimeout(() => console.log("F"), 0);
 // console.log("G");
+
+// const fruits = ["apple", "banana", "apple"];
+
+
+
+const users = [
+  { id: 1, name: "Alex" },
+  { id: 2, name: "Sam" },
+];
+
+
+
+const userMap=users.reduce((acc,user)=>{
+    acc[user.id]=user.name
+    return acc
+},{})
+
+
+console.log(userMap)
