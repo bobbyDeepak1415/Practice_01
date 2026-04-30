1a3ee4d5095f317b9db12179895be3ef41d16d99
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-30 15:25:58 +0530

    setup done

--- index.js
+++ index.js
@@ -25,13 +25,8 @@ function display() {
     obj[i] = (obj[i] || 0) + 1;
   }
 
-  let seen = new Set();
-
   for (let i of str) {
-    if (!seen.has(i)) {
-      result += i + obj[i];
-      seen.add(i);
-    }
+    result += i + obj[i];
   }
 
   return result;
