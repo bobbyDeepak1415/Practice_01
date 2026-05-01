af19f9cdaab9ad97710d6551dbf2babba55d11b0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-02 03:05:53 +0530

    setup done

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-// console.log("A");
-// async function test() {
-//   console.log("B");
+console.log("A");
+async function test() {
+     await Promise.resolve()
+  console.log("B");
 
-//   Promise.resolve().then(()=>console.log("C"))
-//  await Promise.resolve()
-//   console.log("D");
+await  Promise.resolve().then(()=>console.log("C"))
+ await Promise.resolve()
+  console.log("D");
 
-// }
-// test();
-// Promise.resolve().then(() => console.log("E"));
-// setTimeout(() => console.log("F"), 0);
-// console.log("G");
-
-// const fruits = ["apple", "banana", "apple"];
-
-const person1 = {
-  name: "Bobby",
-  age: 30,
-  print(country) {
-    return this.name + " is " + this.age + " years old and lives in "+country
-  },
-};
-
-const person2 = {
-  name: "Deepak",
-  age: 31,
-};
+}
+test();
+Promise.resolve().then(() => console.log("E"));
+setTimeout(() => console.log("F"), 0);
+console.log("G");
 
 
 
-console.log(person1.print())
-
+// A,G,C,B,D,E,F
