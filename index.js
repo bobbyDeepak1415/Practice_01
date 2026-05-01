3a86ea7bd43b91a12fdbfcb9237e29f757d9632c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-02 03:19:32 +0530

    setup done

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-console.log("A");
-async function test() {
-  await Promise.resolve();
-  console.log("B");
+// console.log("A");
+// async function test() {
+//   await Promise.resolve();
+//   console.log("B");
 
-  await Promise.resolve().then(() => console.log("C"));
-  await Promise.resolve();
-  console.log("D");
-}
-test();
-Promise.resolve().then(() => console.log("E"));
-setTimeout(() => console.log("F"), 0);
-console.log("G");
+//   await Promise.resolve().then(() => console.log("C"));
+//   await Promise.resolve();
+//   console.log("D");
+// }
+// test();
+// Promise.resolve().then(() => console.log("E"));
+// setTimeout(() => console.log("F"), 0);
+// console.log("G");
