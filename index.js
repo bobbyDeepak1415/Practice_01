446e27b65b2ab4220eb27933620d4be180103dad
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-05 03:46:33 +0530

    setup done

--- index.js
+++ index.js
@@ -15,17 +15,11 @@
 let nums = [56, 5, 4, 3, 7, 98, 12];
 
 function sortNums(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length - 1 - i; j++) {
-      if (nums[j] > nums[j + 1]) {
-        let temp = nums[j];
-        nums[j] = nums[j + 1];
-        nums[j + 1] = temp;
-      }
-    }
-  }
+  
 
   return nums;
 }
 
 console.log(sortNums(nums));
+
+// console.log(nums.sort((a,b)=>a-b))
