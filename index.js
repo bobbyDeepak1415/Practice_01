77b1a41a70f518e851f5b37188ce1724ded0f53e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-05 03:41:21 +0530

    setup done

--- index.js
+++ index.js
@@ -12,21 +12,20 @@
 // setTimeout(() => console.log("F"), 0);
 // console.log("G");
 
+let nums = [56, 5, 4, 3, 7, 98, 12];
 
-
-
-let nums=[56,5,4,3,7,98,12]
-
-
-function sortNums(nums){
-
-    for(let i=0;i<nums.length;i++){
-        for(let j=1;j<nums.length-1-i;j++){
-            let temp=0
-            if(j)
-        }
+function sortNums(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - 1 - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
     }
+  }
 
+  return nums;
 }
 
-// console.log(sortNums(nums))
+console.log(sortNums(nums));
