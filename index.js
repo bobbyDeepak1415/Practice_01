2e4c6616754fbe4e2a91bd6fdb378c708da07b75
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-05 03:37:26 +0530

    setup done

--- index.js
+++ index.js
@@ -18,8 +18,15 @@
 let nums=[56,5,4,3,7,98,12]
 
 
-function display(){
+function sortNums(nums){
+
+    for(let i=0;i<nums.length;i++){
+        for(let j=1;j<nums.length-1-i;j++){
+            let temp=0
+            if(j)
+        }
+    }
 
 }
 
-// console.log(display(nums))
+// console.log(sortNums(nums))
