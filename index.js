6294519ff16058a26676768ae148462c81280efd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-08 00:14:49 +0530

    setup done

--- index.js
+++ index.js
@@ -14,4 +14,23 @@
 
 
 
-// let arr1=[34,4,12,5,3,2,1]
+let arr1=[34,4,12,5,3,2,1]
+
+
+function display(arr){
+
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                let temp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+            }
+        }
+    }
+
+    return arr
+
+}
+
+console.log(display(arr1))
