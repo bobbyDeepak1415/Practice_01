39ccf65b521f3e3685878d7be6913f2e0ffd427f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-09 00:33:12 +0530

    setup done

--- sum.js
+++ sum.js
@@ -5,22 +5,19 @@ const btn = document.querySelector("button");
 let clickedTimes = 0;
 let runTimes = 0;
 
-const myDebounce = (func, delay) => {
-  let timer = 0;
+const myThrottle = (func, delay) => {
+  let lastRun=new Date().toString()
 
   return function (...args) {
-      clearTimeout(timer);
-    timer=setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
+     
 };
 
-const debounceFunc = myDebounce(() => {
+const throttleFunc = myThrottle(() => {
   run.innerHTML = ++runTimes;
 }, 1000);
 
 btn.addEventListener("click", () => {
   clicked.innerHTML = ++clickedTimes;
-  debounceFunc();
+  throttleFunc();
 });
+
