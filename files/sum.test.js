45c7d5318c9cc2b2391dc26475029c2164c4c521
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-09 00:16:09 +0530

    setup done

--- files/sum.test.js
+++ files/sum.test.js
@@ -1,4 +1,4 @@
-const sum=require('./sum')
+const sum=require('../sum')
 
 
 test("should add the two nums correctly",()=>{
---
+++ sum.js
@@ -0,0 +1,18 @@
+
+
+const clicked=document.querySelector(".clicked")
+const run=document.querySelector(".func_run")
+const btn=document.querySelector("button")
+
+let clickedTimes=0
+let runTimes=0
+
+
+
+
+btn.addEventListener("click",()=>{
+
+})
+
+
+
