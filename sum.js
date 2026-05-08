285bf5dd551a8228dee36f4e0bacf081bf1932f3
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-09 03:32:06 +0530

    setup done

---
+++ sum.js
@@ -0,0 +1,13 @@
+
+
+const clicked=document.querySelector(".clicked")
+const run=document.querySelector(".func_run")
+const button=document.querySelector("button")
+
+
+let clickedTimes=0
+
+
+button.addEventListener("click",()=>{
+    console.log("clicked")
+})
