c1d5e6dbf9cc7e30535c5245325d9ee7d78143b7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-09 00:22:53 +0530

    setup done

--- sum.js
+++ sum.js
@@ -1,18 +1,26 @@
+const clicked = document.querySelector(".clicked");
+const run = document.querySelector(".func_run");
+const btn = document.querySelector("button");
 
+let clickedTimes = 0;
+let runTimes = 0;
 
-const clicked=document.querySelector(".clicked")
-const run=document.querySelector(".func_run")
-const btn=document.querySelector("button")
+const myDebounce = (func, delay) => {
+  let timer = 0;
 
-let clickedTimes=0
-let runTimes=0
+  return function (...args) {
+    setTimeout(() => {
+      clearTimeout(timer);
+      func(...args);
+    }, delay);
+  };
+};
 
+const debounceFunc = myDebounce(() => {
+  run.innerHTML = ++runTimes;
+}, 1000);
 
-
-
-btn.addEventListener("click",()=>{
-clickedTimes++
-})
-
-
-
+btn.addEventListener("click", () => {
+  clicked.innerHTML = ++clickedTimes;
+  debounceFunc();
+});
