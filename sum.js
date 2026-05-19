46c17de7f9a925dc4bd1f08f069d0a4b628e58f1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-20 02:49:56 +0530

    setup done

--- sum.js
+++ sum.js
@@ -1,11 +1,23 @@
 
 
-// console.log("Hello")
 
-let str="Hello world from Bobby"
+let str="hello world from bobby"
 
-function display(){
 
+function display(str){
+    
+    let arr=str.split(" ")
+    let result=[]
+
+    for(let i of arr){
+        let capWord=i.charAt(0).toUpperCase()+word.slice(1)
+        let reverseWord=capWord.split("").reverse().join("")
+        result.push(reverseWord)
+    }
+    
+    return result
+    
 }
 
-// console.log(display(str))
+console.log(display(str))
+// o/p= 
