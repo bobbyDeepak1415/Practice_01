c87f611a297f4aeb74b93d2fc85c540038f87605
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-25 15:49:48 +0530

    set up done

--- practice.jsx
+++ practice.jsx
@@ -1,31 +1,17 @@
-function checkFrequency (arr1, arr2)  {
-  if (arr1.length !== arr2.length) return false;
 
-  let f1 = {};
-  let f2 = {};
 
-  for (let val of arr1) {
-    f1[val] = (f1[val] || 0) + 1;
+function display(greeting){
+
+  return function(name){
+
+    return `${greeting} there ${name}`
+
   }
 
-  for (let val of arr2) {
-    f2[val] = (f2[val] || 0) + 1;
-  }
-
-//   console.log(f1, f2);
-
-  for (let val1 of arr1) {
-    let square = val1 * val1;
-    if (!f2[square]) {
-      return false;
-    }
-  }
-
-  return f1
-};
 
 
-checkFrequency([1, 2, 3], [1,4,9]); // false
-checkFrequency([], []) // false
+}
 
+const call1=display("Hello")
 
+console.log(call1("Bobby"))
