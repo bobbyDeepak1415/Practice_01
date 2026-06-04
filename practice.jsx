1293d532992b519eb8e0f55ae5cb9f50437c4577
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-05 03:24:34 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,18 +1,1 @@
-function display() {
-  let count = 0;
 
-  return {
-    increment: () => {
-      return ++count;
-    },
-    decrement: () => {
-      return --count;
-    },
-  };
-}
-const counter=display()
-
-
-console.log(counter.decrement())
-console.log(counter.increment())
-console.log(counter.increment())
