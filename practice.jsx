9ce0c03013171a9d8d1d92e8f431bd16311a9b2e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-05 19:34:25 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,15 +1,21 @@
-
-
 // let str="{{[({)([}])]}"
 
+let arr = [2, 3, 1, 4, 5, 9, 14, 10];
 
+function display(arr) {
+  // let result=[]
 
-// let arr=[2,3,1,4,5,9,14,10]
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
 
-
-
-function display(){
-
+  return arr;
 }
 
-
+console.log(display(arr));
