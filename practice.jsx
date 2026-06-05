e67d13bbf6c830a575b65228cf3e5352c93f8a1f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-06 02:21:44 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,18 @@
+function display() {
+  let fName = "Bobby";
 
+  return function () {
+    console.log(this.fName);
+  };
+}
 
+display();
 
+const display1 = () => {
+  let fName = "Bob";
+  return function () {
+    console.log(this.fName);
+  };
+};
+
+display1();
