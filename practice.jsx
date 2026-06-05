7279d017fd7328f79db0090df5d9663b0a834dac
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-06 01:32:41 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -2,3 +2,21 @@
 
 let arr = [2, 3, 1, 4, 5, 9, 14, 10];
 
+function display(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j + 1]<arr[j]) {
+        let temp = arr[j + 1];
+
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(display(arr));
