9f6a068e3c5d9e99eeebbf5ef3e9f98b98fdcdb1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-17 16:18:56 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,15 +1,26 @@
 
 
 
-const arr=[23,1,6,9,7]
+const arr=[23,1,6,9,17]
 
 
-function display(){
+function display(arr,target){
+
+    let result=[]
+
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]===target)
+result.push(i,j)
+        }
+    }
+
+    return result
 
 }
 
 
-// console.log(display(arr,7))
+console.log(display(arr,7))
 
 
 
