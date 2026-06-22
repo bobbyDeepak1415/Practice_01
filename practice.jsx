0ebe2bcf871f92112eb4f6da04d69245e536ac7e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-23 00:29:14 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,15 +1,3 @@
-let person1 = {
-  title: "Bob",
-  age: 30,
-  active: true,
-  print(city) {
-    console.log(
-      `${this.title} is ${this.age} years old and status is ${this.active} and is from ${city}`,
-    );
-  },
-};
 
 
-const naaam=person1.title
 
-console.log(naaam)
