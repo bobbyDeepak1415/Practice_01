caf85a2458f744d7e7b726dc32b1f580cebf5ff0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-29 03:37:53 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,18 +1,2 @@
 
 
-console.log("A");
-async function test() {
-  await Promise.resolve(); 1
-  console.log("B");
-
-  await Promise.resolve().then(() => console.log("C")); 2
-  await Promise.resolve();
-  console.log("D");
-}
-test();
-Promise.resolve().then(() => console.log("E"));
-setTimeout(() => console.log("F"), 0);
-console.log("G");
-
-
-// A,G,B,E,C,D,F
