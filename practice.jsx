395e02cecf19b5d6d761437badeaa90d08027604
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-28 20:31:38 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -15,3 +15,10 @@
 // // Expected output: [object Promise]
 
 
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+wait(0).then(() => console.log(4));
+Promise.resolve()
+  .then(() => console.log(2))
+  .then(() => console.log(3));
+console.log(1); // 1, 2, 3, 4
