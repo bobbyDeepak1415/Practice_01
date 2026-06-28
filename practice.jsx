2b0aa5d13427fd88c45cb8c59098fcf2e257ebba
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-29 00:55:57 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -22,17 +22,5 @@
 
 const promise = new Promise((resolve, reject) => {
   console.log("Promise callback"); 
-  resolve();
-}).then((result) => {
-  console.log("Promise callback (.then)");
-});
 
-setTimeout(() => {
-  console.log("event-loop cycle: Promise (fulfilled)", promise);
-}, 0);
-
-console.log("Promise (pending)", promise);
-
-
-
-// Promise callback,Promise (pending),Promise callback (.then),event-loop cycle: Promise (fulfilled)", promise
+})
