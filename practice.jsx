b188147f404b5b77770b0f1f8ed9f29b5cfd3781
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-28 20:52:20 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,20 +1,34 @@
-// // Creating a Promise around an old callback API
+// // // Creating a Promise around an old callback API
 
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("foo");
-  }, 300);
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("foo");
+//   }, 300);
+// });
+
+// promise1.then((value) => {
+//   console.log(value);
+//   // Expected output: "foo"
+// });
+
+// console.log(promise1);
+// // Expected output: [object Promise]
+
+// const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// ************************************************************************************************8
+
+//// Dont open terminal before guessing. first look at the chat in gemini and then revise and then guess
+
+const promise = new Promise((resolve, reject) => {
+  console.log("Promise callback");
+  resolve();
+}).then((result) => {
+  console.log("Promise callback (.then)");
 });
 
-promise1.then((value) => {
-  console.log(value);
-  // Expected output: "foo"
-});
+setTimeout(() => {
+  console.log("event-loop cycle: Promise (fulfilled)", promise);
+}, 0);
 
-console.log(promise1);
-// Expected output: [object Promise]
-
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-
-
+console.log("Promise (pending)", promise);
