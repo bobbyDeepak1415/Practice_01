0a2402c1bd8cc7db987584bdb6a9c660e1ae64d8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-28 20:12:57 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,17 @@
 
 
 console.log("hello bruv")
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+});
+
+promise1.then((value) => {
+  console.log(value);
+  // Expected output: "foo"
+});
+
+console.log(promise1);
+// Expected output: [object Promise]
