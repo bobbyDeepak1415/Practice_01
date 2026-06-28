11668c3db3c9b641cd320725cf7cf7db9cb4fc95
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-29 03:24:05 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,2 +1,22 @@
+console.log("Alpha");
 
+new Promise((resolve) => {
+  console.log("Beta");
+  resolve();
+}).then(() => {
+  console.log("Gamma");
+});
+
+setTimeout(() => {
+  console.log("Delta");
+
+  new Promise((resolve) => {
+    console.log("Epsilon");
+    resolve();
+  }).then(() => {
+    console.log("Zeta");
+  });
+}, 0);
+
+console.log("Eta");
 
