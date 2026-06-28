ecadd305dc9fe84882a559124bccc0264ca40fc9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-29 03:09:08 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,16 +1,2 @@
-// Creating a Promise around an old callback API
 
-const promise = new Promise((resolve, reject) => {
-  console.log("Promise callback"); //1
-  setTimeout(() => {
-    resolve();
-  }, 1000);
-}).then((result) => {
-  console.log("Promise callback (.then)"); //4
-});
 
-setTimeout(() => {
-  console.log("event-loop cycle: Promise (fulfilled)"); //3
-}, 0);
-
-console.log("Promise (pending)",);//2
