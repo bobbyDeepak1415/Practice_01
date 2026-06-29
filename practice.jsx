5676b0d9f28a16da0e09cb2cdd618ac9f5d6cba0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-29 17:12:41 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,19 +1,34 @@
+console.log("1");
 
+setTimeout(() => {
+  console.log("2");
+}, 0);
 
-console.log("A");
-async function test() {
-  await Promise.resolve(); 1
-  console.log("B");
-
-  await Promise.resolve().then(() => console.log("C")); 2
+async function ultimate() {
+  console.log("3");
   await Promise.resolve();
-  console.log("D");
+  console.log("4");
 }
-test();
-Promise.resolve().then(() => console.log("E"));
-setTimeout(() => console.log("F"), 0);
-console.log("G");
+
+ultimate();
+
+new Promise((resolve) => {
+  console.log("5");
+  resolve();
+})
+  .then(() => {
+    console.log("6");
+  })
+  .then(() => {
+    console.log("7");
+  });
+
+console.log("8");
+
+
+// macro- 
+// micro-
 
 
 
-// A,G,B,D,E,C,F
+// sync-
