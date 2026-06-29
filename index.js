df923116ebbdb1ebeb33628e55826d0717fa5359
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-29 17:16:37 +0530

    setup done

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// 1
+// console.log("1");
+
+// setTimeout(() => {
+//   console.log("2");
+// }, 0);
+
+// async function ultimate() {
+//   console.log("3");
+//   await Promise.resolve();
+//   console.log("4");
+// }
+
+// ultimate();
+
+// new Promise((resolve) => {
+//   console.log("5");
+//   resolve();
+// })
+//   .then(() => {
+//     console.log("6");
+//   })
+//   .then(() => {
+//     console.log("7");
+//   });
+
+// console.log("8");
+
+// -----------------------
+
+// 2.
 // console.log("A");
 // async function test() {
 //   await Promise.resolve(); 1
@@ -11,7 +42,3 @@
 // Promise.resolve().then(() => console.log("E"));
 // setTimeout(() => console.log("F"), 0);
 // console.log("G");
-
-
-
-// A,G,B,D,E,C,F
