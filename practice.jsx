78f6da7240e036665b28fba0251a64f030802c16
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-16 02:21:29 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,16 +1,2 @@
 
 
-
-const prom=new Promise((resolve,reject)=>{
-  console.log("hello")
-  resolve()
-})
-
-
-prom.then(()=>{
-  console.log("bye")
-}).catch((err)=>{
-  console.log('error',err)
-})
-
-
