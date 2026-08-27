52cb727b495528e8fe977cdaf3bad6a57e60ee83
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-28 02:21:02 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,30 @@
 
 
-console.log("hello")
+
+let obj1={
+    name:"Bobby",
+    age:30,
+
+    display(city,state){
+return this.name+" is "+this.age+" years old and is from "+city+" in "+state
+    }
+}
+
+
+let obj2={
+    name:"Jon",
+    age:45
+}
+
+
+// console.log(obj1.display())
+const print=obj1.display.apply(obj2,["ny","DC"])
+
+
+
+
+
+
+
+
+
