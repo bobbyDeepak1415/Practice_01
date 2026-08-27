32ef1ee035c40279926b4c07bdb85c2125513caa
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-28 02:23:55 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,30 +1,16 @@
 
 
 
-let obj1={
-    name:"Bobby",
-    age:30,
 
-    display(city,state){
-return this.name+" is "+this.age+" years old and is from "+city+" in "+state
-    }
+function display(){
+    let name="Bobby"
+return function(){
+    return `${name} is my name`
+}
 }
 
 
-let obj2={
-    name:"Jon",
-    age:45
-}
-
-
-// console.log(obj1.display())
-const print=obj1.display.bind(obj2,"ny","DC")
+const print=display()
 
 console.log(print())
-
-
-
-
-
-
 
