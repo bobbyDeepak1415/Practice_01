289f743a3b632fe224541f9c8c9e098fa2345243
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-28 02:28:46 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,3 @@
 
 
-let city="ongole"
-
-function display(){
-    let name="Bobby"
-    console.log(`${name} is from ${city}`)
-return function(age){
-    return `${name} is ${age} years old`
-}
-}
-
-
-const print=display()
-
-console.log(print(30))
 
